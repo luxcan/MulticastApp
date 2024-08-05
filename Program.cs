62581baf00165124538429b5f3aed77ee0e9f0aa
luxcan <tatong27@gmail.com>
2024-08-05 19:53:40 +0800

    Update to allow user to select which IP to send/receive

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Net.NetworkInformation;
 
 string multicastGroup;
 int multicastPort;
 int mode;
 bool quit = false;
+
+// List all available relevant IP addresses
+List<IPAddress> localIPs = new List<IPAddress>();
+int ipIndex = 1;
+
+Console.WriteLine("Select a local IP address for sending/receiving:");
+foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces()) {
+    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses) {
+        if (ip.Address.AddressFamily == AddressFamily.InterNetwork &&
+            !IPAddress.IsLoopback(ip.Address) &&
+            !ip.Address.ToString().StartsWith("169.254")) {
+            localIPs.Add(ip.Address);
+            Console.WriteLine($"{ipIndex++}. {ip.Address}");
+        }
+    }
+}
+
+IPAddress localIP = null!;
+while (true) {
+    Console.Write("Enter the number of the IP address: ");
+    if (int.TryParse(Console.ReadLine(), out int ipChoice) && ipChoice > 0 && ipChoice <= localIPs.Count) {
+        localIP = localIPs[ipChoice - 1];
+        break;
+    }
+    Console.WriteLine("Invalid choice. Please select a valid number.");
+}
 
 while (true) {
     Console.Write("Enter the multicast IP address: ");
@@ -47,13 +74,13 @@ Thread keyListener = new Thread(() => {
 keyListener.Start();
 
 if (mode == 1) {
-    StartSender(multicastGroup, multicastPort, ref quit);
+    StartSender(localIP, multicastGroup, multicastPort, ref quit);
 } else {
-    StartReceiver(multicastGroup, multicastPort, ref quit);
+    StartReceiver(localIP, multicastGroup, multicastPort, ref quit);
 }
 
-static void StartSender(string multicastGroup, int multicastPort, ref bool quit) {
-    using (var udpClient = new UdpClient()) {
+static void StartSender(IPAddress localIP, string multicastGroup, int multicastPort, ref bool quit) {
+    using (var udpClient = new UdpClient(new IPEndPoint(localIP, 0))) {
         IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(multicastGroup), multicastPort);
         Console.Write("Enter the message to send: ");
         string message = Console.ReadLine()!;
@@ -64,19 +91,17 @@ static void StartSender(string multicastGroup, int multicastPort, ref bool quit)
 
         while (!quit) {
             udpClient.Send(data, data.Length, remoteEndPoint);
-            Console.WriteLine($"{DateTime.Now} = Send");
+            Console.WriteLine($"{DateTime.Now} = Sent");
             Thread.Sleep(1000);
         }
     }
 }
 
-static void StartReceiver(string multicastGroup, int multicastPort, ref bool quit) {
-    using (UdpClient udpClient = new UdpClient()) {
+static void StartReceiver(IPAddress localIP, string multicastGroup, int multicastPort, ref bool quit) {
+    using (UdpClient udpClient = new UdpClient(new IPEndPoint(localIP, multicastPort))) {
         udpClient.ExclusiveAddressUse = false;
 
-        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, multicastPort);
         udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-        udpClient.Client.Bind(localEndPoint);
 
         udpClient.JoinMulticastGroup(IPAddress.Parse(multicastGroup));
 
@@ -84,14 +109,15 @@ static void StartReceiver(string multicastGroup, int multicastPort, ref bool qui
 
         while (!quit) {
             if (udpClient.Available > 0) {
-                byte[] data = udpClient.Receive(ref localEndPoint);
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, multicastPort);
+                byte[] data = udpClient.Receive(ref remoteEndPoint);
                 try {
                     string message = Encoding.UTF8.GetString(data);
-                    Console.WriteLine($"{DateTime.Now} = Received from \"{localEndPoint.Address}:{localEndPoint.Port}\": {message}");
+                    Console.WriteLine($"{DateTime.Now} = Received from \"{remoteEndPoint.Address}:{remoteEndPoint.Port}\": {message}");
                 } catch (DecoderFallbackException) {
-                    Console.WriteLine($"{DateTime.Now} = Received (Not in UTF8) from \"{localEndPoint.Address}:{localEndPoint.Port}\": Bytes Length ({data.Length})");
+                    Console.WriteLine($"{DateTime.Now} = Received (Not in UTF8) from \"{remoteEndPoint.Address}:{remoteEndPoint.Port}\": Bytes Length ({data.Length})");
                 } catch (Exception) {
-                    Console.WriteLine($"{DateTime.Now} = Received from \"{localEndPoint.Address}:{localEndPoint.Port}\": Encounter exception while trying to convert data into UTF8");
+                    Console.WriteLine($"{DateTime.Now} = Received from \"{remoteEndPoint.Address}:{remoteEndPoint.Port}\": Encounter exception while trying to convert data into UTF8");
                 }
             }
         }
